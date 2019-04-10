9e0606664b9b345c212f4b99dad6fabb484feca7
Bryan <bcromas@gmail.com>
2019-04-10 08:20:20 -0400

    more practice

--- Apple_and_Orange.py
+++ Apple_and_Orange.py
@@ -8,7 +8,21 @@ import sys
 
 # Complete the countApplesAndOranges function below.
 def countApplesAndOranges(s, t, a, b, apples, oranges):
-    print(s,t,a,b,apples,oranges)
+    apples_hit_house = 0
+    oranges_hit_house = 0
+
+    for i in apples:
+        i_from_tree = i+a
+        if i_from_tree >= s and i_from_tree <= t:
+            apples_hit_house += 1
+
+    for i in oranges:
+        i_from_tree = i+b
+        if i_from_tree >= s and i_from_tree <= t:
+            oranges_hit_house += 1
+
+    # return '{}\n{}'.format(apples_hit_house, oranges_hit_house)
+    print('{}\n{}'.format(apples_hit_house, oranges_hit_house))
 
 if __name__ == '__main__':
     # st = input().split()
@@ -17,15 +31,15 @@ if __name__ == '__main__':
     s = 7
 
     # t = int(st[1]) #t: integer, ending location of Sam's house location.
-    t = 11
+    t = 10
 
     # ab = input().split()
 
     # a = int(ab[0]) #a: integer, location of the Apple tree.
-    a = 5
+    a = 4
 
     # b = int(ab[1]) #b: integer, location of the Orange tree.
-    b = 15
+    b = 12
 
     # mn = input().split()
 
@@ -34,9 +48,9 @@ if __name__ == '__main__':
     # n = int(mn[1])
 
     # apples = list(map(int, input().rstrip().split())) #contains m space-separated integers denoting the respective distances that each apple falls from point a.
-    apples = [-2,2,1]
+    apples = [2,3,-4]
 
     # oranges = list(map(int, input().rstrip().split())) #contains n space-separated integers denoting the respective distances that each orange falls from point b.
-    oranges = [5,-6]
+    oranges = [3,-2,-4]
 
-    countApplesAndOranges(s, t, a, b, apples, oranges)
+    print(countApplesAndOranges(s, t, a, b, apples, oranges))
