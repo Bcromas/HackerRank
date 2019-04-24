771c23a15c702dbd47b9d587c173fe11095c7349
Bryan <bcromas@gmail.com>
2019-04-24 09:51:35 -0400

    commented out var

--- Between_Two_Sets.py
+++ Between_Two_Sets.py
@@ -7,21 +7,24 @@ import sys
 # Complete the getTotalX function below.
 #
 def getTotalX(a, b):
-    # min_a = min(a)
-    # max_a = max(a)
-
-    # min_b = min(b)
-    # max_b = max(b)
-
-    # min_both = min(min_a, min_b)
-    # max_both = max(max_a, max_b)
-
-    # print(min_both, max_both)
-
+    
+    #generate lists of ints that are products of an element in array a 
+    list_X = []
     for i in a:
-        for j in range(100):
-            print(i, j)
+        list_i = []
+        for j in range(1,101):
+            list_i.append(i*j)
+        list_X.append(list_i)
 
+    #idea is to iterate through lists in list_X & find intersection of individual list members
+    # for i in list_X:
+        # intersect_i = set(i).intersection(i+1)
+        
+
+    # intersect = set(list_a).intersection(list_b)
+    # intersect_list = list(intersect)
+    # intersect_list_sorted = sorted(intersect_list)
+    # print(intersect_list_sorted)
 
 if __name__ == '__main__':
     # f = open(os.environ['OUTPUT_PATH'], 'w')
@@ -40,7 +43,7 @@ if __name__ == '__main__':
     # b = list(map(int, input().rstrip().split()))
     b = [16,32,96]
 
-    total = getTotalX(a, b)
+    # total = getTotalX(a, b)
     # print(total)
 
     # f.write(str(total) + '\n')
